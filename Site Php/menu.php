4abf446e2ee18522d9a131547fe9df313be780f9
RICARDO DO PRADO LOURENCO DE SOUZA <ricardo.1935@aluno.pr.senac.br>
2023-11-22 15:14:32 -0300

    att

---
+++ Site Php/menu.php
@@ -0,0 +1,33 @@
+<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+  <img src="img/logo.png" width=70px>
+    <a class="navbar-brand" href="#">Pobre Viagens</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Passagens</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Lugares
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Praias</a></li>
+            <li><a class="dropdown-item" href="#">Cidades</a></li>
+            <li><a class="dropdown-item" href="#">Pontos Turisticos</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Pesquise..." aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Pesquisar</button>
+      </form>
+    </div>
+  </div>
+</nav>
---
+++ Site Php/rodape.php
@@ -0,0 +1,12 @@
+<div class="rodape"> </div>
+    <footer>
+	<p>&copy; 2023 - Todos os direitos reservados para Ricardo</p>
+	<p>Entre em contato pelo e-mail: contato@exemplo.com</p>
+	<nav>
+		<ul>
+			<a href="#">Termos de uso</a></li>
+			<a href="#">Política de privacidade</a></li>
+			<a href="#">Sobre nós</a></li>
+		</ul>
+	</nav>
+</footer>
