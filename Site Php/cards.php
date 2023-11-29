a3b3a0534e3986035b03502550559f23351c0841
RICARDO DO PRADO LOURENCO DE SOUZA <ricardo.1935@aluno.pr.senac.br>
2023-11-29 14:27:32 -0300

    att

--- Site Php/cards.php
+++ Site Php/cards.php
@@ -3,10 +3,10 @@
     <div class="row row row-cols-1 row-cols-md-3 g-3"> <!-- A class row deixa os elementos em linha -->
       <div class="col">
         <div class="card" style="width: 18rem;">
-          <img src="img/iguaçu.webp" class="card-img-top" alt="...">
+          <img src="img/iguaçu1.webp" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Cataratas do Iguaçu, PR</h5>
-            <p class="card-text">Elas foram consideradas em uma votação popular mundial, como umas novas 7 Maravilhas Naturais do Mundo. Mas elas não exclusivamente brasileiras, parte das quedas se encontra em território brasileiro, parte em território argentino</p>
+            <p class="card-text">Elas foram consideradas em uma votação popular mundial, como umas novas 7 Maravilhas Naturais do Mundo. Mas elas não exclusivamente brasileiras</p>
             <a href="#" class="btn btn-primary">Veja mais</a>
           </div>
         </div>
@@ -15,7 +15,7 @@
         <div class="card" style="width: 18rem;">
           <img src="img/rota.webp" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Rota das Emoções: Lençóis Maranhenses, MA</h5>
+            <h5 class="card-title">Rota das Emoções, MA</h5>
             <p class="card-text">O nome não podia ser mais adequado. Muita gente recomenda que o melhor sentido para fazer a Rota das Emoções é começar em Jeri (Ceará)</p>
             <a href="#" class="btn btn-primary">Veja mais</a>
           </div>
@@ -25,18 +25,19 @@
         <div class="card" style="width: 18rem;">
           <img src="img/espelho.webp" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Costa do Descobrimento, Bahia</h5>
-            <p class="card-text">A região que encantou os portugueses há mais de 500 anos continua encantando gente do mundo inteiro.Cercada por diversos atrativos naturais como praias, baías, recifes de coral, manguezais e rios navegáveis</p>
+            <h5 class="card-title">Costa do Descobrimento</h5>
+            <p class="card-text">A região que encantou os portugueses há mais de 500 anos continua encantando gente do mundo inteiro.Cercada por diversos atrativos naturais</p>
             <a href="#" class="btn btn-primary">Veja mais</a>
           </div>
         </div>
       </div>
     </div> <!-- FIM do row 1-->
+    <div class="espaco2"> </div>
     <div class="b-example-divider"></div>
     <div class="row">
       <div class="col">
         <div class="card" style="width: 18rem;">
-          <img src="img/paraty.webp" class="card-img-top" alt="...">
+          <img src="img/paraty1.webp" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">Paraty, Rio de Janeiro</h5>
             <p class="card-text">A riqueza natural combinada com seu patrimônio histórico e cultural , rendeu a Paraty (em conjunto com Ilha Grande), o título de Patrimônio Mundial da Unesco.</p>
@@ -46,9 +47,9 @@
       </div>
       <div class="col">
         <div class="card" style="width: 18rem;">
-          <img src="img/jordao.webp" class="card-img-top" alt="...">
+          <img src="img/jordao1.webp" class="card-img-top" alt="...">
           <div class="card-body">
-            <h5 class="card-title">Campos do Jordão e a Serra da Mantiqueira, SP</h5>
+            <h5 class="card-title">Campos do Jordão</h5>
             <p class="card-text">Hospedagens de alta qualidade, restaurantes de ótima qualidade e a paisagem privilegiada da região, fazem desse tipo de viagem uma escolha certeira.</p>
             <a href="#" class="btn btn-primary">Veja mais</a>
           </div>
